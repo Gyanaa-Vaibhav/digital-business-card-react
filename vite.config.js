0ea5b0ee9198725f167b591e6b736b49ce5807a0
Gyanaa Vaibhav <gynanrudr0@gmail.com>
2024-10-21 14:54:39 +0530

    Push Final Changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/digital-busniess-card-react/",
+  base: "/digital-business-card-react/",
   plugins: [react()],
-})
+});
